005a2c6778356e939234fb7b58f6540ed0a16b10
pawelek <k.pawelek@vp.pl>
2024-01-31 15:33:18 +0100

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,22 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-const galleryList = document.querySelector(".gallery");
+const galleryList = document.querySelector('.gallery');
 
 const galleryItem = galleryItems
   .map(
-    (item) => `<li class="gallery__item">
+    item => `<li class="gallery__item">
 <a class="gallery__link" href="${item.original}">
 <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}"/>
 </a>
 </li>`
   )
-  .join("");
+  .join('');
 
 galleryList.innerHTML = galleryItem;
 
-var lightbox = new SimpleLightbox(".gallery a", {
+var lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
-  captionsData: "alt",
+  captionsData: 'alt',
 });
 
 // // Change code below this line
