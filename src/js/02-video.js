4713439e8afcef203771051441764587d820ee75
pawelek <k.pawelek@vp.pl>
2024-02-01 11:44:39 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const currentTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+player.on('timeupdate', currentTime);
+
+const getTime = localStorage.getItem('videoplayer-current-time');
+if (getTime) {
+  player.setCurrentTime(parseFloat(getTime));
+}
